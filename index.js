d86144cec850a52fceafded0e63c99af8a9a2455
Khoa Minh <khoanguyenminh222@gmail.com>
2024-04-03 15:52:40 +0700

    1

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use("/api/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log('Server is running on port 8080');
+  console.log(`Server is running on port ${PORT}`);
 });
